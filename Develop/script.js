f55b243285b01b9e12241c0ebafcdfc004c91d7d
Andrew Faugno <andrewfaugno825@gmail.com>
2022-04-12 23:10:33 -0400

    length can only be 8-128

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,32 @@
 // button location
 var generateBtn = document.querySelector("#generate");
-var copyBtn = document.querySelector("#copy")
-
+var copyBtn = document.querySelector("#copy");
 
 var numbers = "0123456789";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var special = "~`! @#$%^&*()_-+={[}]|:\;\"'<,>.?/";
+var special = "~`!@#$%^&*()_-+={[}]|'<,>.?/";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var combineAll = numbers.concat(lowerCase, special, upperCase);
 
-
 // generate password with different options
 function writePassword() {
-  var passwordLength = window.prompt("Enter Password Length (8-128)");
-  var password = "";
-  for (var i = 0; i < passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * combineAll.length);
-    password += combineAll.substring(randomNumber, randomNumber + 1);
-  }  
+  var passwordLength = parseInt(prompt("Enter Password Length (8-128)"));
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Please enter valid option");
+    writePassword();
+  } else {
+    var password = "";
+    for (var i = 0; i < passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * combineAll.length);
+      password += combineAll.substring(randomNumber, randomNumber + 1);
+      
+      // asigns print location to var
+      var passwordText = document.querySelector("#password");
 
-  // asigns print location to var
-  var passwordText = document.querySelector("#password");
-
-  // prints password in box
-  passwordText.value = password;
-
+      // prints password in box
+      passwordText.value = password;
+    }
+  }
 }
 
 // copies password to clipboard
@@ -38,9 +40,4 @@ function copyPassword() {
 // run function on generate password button click
 generateBtn.addEventListener("click", writePassword);
 // run function on copy button click
-copyBtn.addEventListener("click", copyPassword)
-
-
-
-
-
+copyBtn.addEventListener("click", copyPassword);
