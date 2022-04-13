d79eaafafa373c523b86c59090baa6115f8b1236
Andrew Faugno <andrewfaugno825@gmail.com>
2022-04-13 13:46:48 -0400

    comments to js

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,25 @@
 // button location
 var generateBtn = document.querySelector("#generate");
 var copyBtn = document.querySelector("#copy");
+
+// checkboxes locations
 var upperCb = document.querySelector("#check-upper");
 var lowerCb = document.querySelector("#check-lower");
 var numberCb = document.querySelector("#check-number");
 var specialCb = document.querySelector("#check-special");
 
+// password length number location (above slider)
 var output = document.getElementById("counter");
 
+// changes number when slider is moved
 var slider = document.getElementById("myRange").oninput = function() {
   var value = (this.value - this.min)/(this.max - this.min)*100;
   output.innerHTML = this.value;
 }
 
-
-
-
 // generate password with different options
 function writePassword() {
+
     // check if checkboxes are checked then either add them or not to passwordString
     if (upperCb.checked) {
       var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -42,15 +44,17 @@ function writePassword() {
     } else {
       var special = "";
     }
-  
+
+    // final passwordString var
     var passwordString = lowerCase.concat(upperCase, special, numbers);
 
+    // picks a random char from string n times
     var password = "";
     for (var i = 0; i < counter.textContent; i++) {
       var randomNumber = Math.floor(Math.random() * passwordString.length);
       password += passwordString.substring(randomNumber, randomNumber + 1);
       
-      // asigns print location to var
+      // assigns print location to var
       var passwordText = document.querySelector("#password");
 
       // prints password in box
