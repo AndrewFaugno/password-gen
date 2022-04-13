de0a3555c1fe2a71d9545784186a60b6d76b57ce
Andrew Faugno <andrewfaugno825@gmail.com>
2022-04-12 22:37:23 -0400

    generates password with changeable length

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,46 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// button location
 var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy")
 
-// Write password to the #password input
+
+var numbers = "0123456789";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var special = "~`! @#$%^&*()_-+={[}]|:\;\"'<,>.?/";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var combineAll = numbers.concat(lowerCase, special, upperCase);
+
+
+// generate password with different options
 function writePassword() {
-  var password = generatePassword();
+  var passwordLength = window.prompt("Enter Password Length (8-128)");
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * combineAll.length);
+    password += combineAll.substring(randomNumber, randomNumber + 1);
+  }  
+
+  // asigns print location to var
   var passwordText = document.querySelector("#password");
 
+  // prints password in box
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+// copies password to clipboard
+function copyPassword() {
+  var copyText = document.getElementById("password");
+  copyText.select();
+  document.execCommand("copy");
+  window.alert("Copied to Clipboard!");
+}
+
+// run function on generate password button click
 generateBtn.addEventListener("click", writePassword);
+// run function on copy button click
+copyBtn.addEventListener("click", copyPassword)
+
+
+
+
+
